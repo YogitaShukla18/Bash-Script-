137a4fe3d54dd8b1aa88514113231f2435056640
YOGITA SHUKLA <123798309+YogitaShukla18@users.noreply.github.com>
2023-12-19 19:08:26 +0530

    activeshell.sh

--- activeshell.sh
+++ activeshell.sh
@@ -5,11 +5,4 @@ read FIRST_NAME
 echo What is your last name?
 read LAST_NAME
 
-echo Hello $FIRST_NAME $LAST_NAME#!/bin/bash
-
-echo What is your first name?
-read FIRST_NAME
-echo What is your last name?
-read LAST_NAME
-
 echo Hello $FIRST_NAME $LAST_NAME
